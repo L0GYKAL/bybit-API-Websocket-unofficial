2cba9b9761cab998e3aa0c243224a34605fab41d
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-06-07 11:47:19 +0200

    Update bybitAPI.py

--- bybitAPI.py
+++ bybitAPI.py
@@ -6,26 +6,34 @@ import hashlib
 import binascii
 import json
 
-api_key = ""
-secret = ""
-expires = time.now()+1000
-signature = create_sha256_signature(secret, 'GET/realtime' + expires)
-symbol = 'BTCUSD'
+class bybit(api_key: str, secret: str):
+    def __init__(self):
+        self.secret = secret
 
-def create_sha256_signature(key, message):
-    byte_key = binascii.unhexlify(key)
-    message = message.encode()
-    return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
+    def signature(self, key, message):
+        byte_key = binascii.unhexlify(key)
+        message = message.encode()
+        return hmac.new(byte_key, message, hashlib.sha256).hexdigest().upper()
 
-async def fetchPrice():
-    async with websockets.connect(
-            'ws://stream.bybit.com/realtime') as websocket:
-        #auth
-        await websocket.send(str('{"op":"auth","args":[{' + api_key + '},expires,{' + signature + '}]}'))
-        
+    async def fetchPrice(self, symbol: str):
+        expires = time.now()+1000
+        signature = signature(self, self.secret, 'GET/realtime' + expires)
+        async with websockets.connect(
+                'ws://stream.bybit.com/realtime') as websocket:
+            #auth
+            await websocket.send(str('{"op":"auth","args":[{' + api_key + '},expires,{' + signature + '}]}'))
 
-        await websocket.send('{"op":"subscribe","args":["instrument.' + symbol + '"]}')
-        symbolInfo = await websocket.rcv()
-        print('The last price for ' + symbol + ' is ' + str(symbolInfo['data']['last_price']))
 
-asyncio.get_event_loop().run_until_complete(fetchPrice())
+            await websocket.send('{"op":"subscribe","args":["instrument.' + symbol + '"]}')
+            symbolInfo = await websocket.rcv()
+            print('The last price for ' + symbol + ' is ' + str(symbolInfo['data']['last_price']))
+            return symbolInfo
+    def getPriceInfo(symbol: str)
+        symbolInfo = asyncio.get_event_loop().run_until_complete(fetchPrice(self, symbol))
+
+if __name__ == "__main__":
+    key = ''
+    secret = ''
+    symbol = 'BTCUSD'
+    bybit = bybit(key, secret)
+    price = bybit.getPriceInfo(symbol)
